060e68ad84f9cd9eaeb3df238bc247bc7db13af5
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-08-24 12:10:25 +0530

    Reached to the boiler plate for the server side and used express.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("object");
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+    res.json('You are in root folder.');
+})
+
+app.listen(port,() => {
+    console.log('server started on port ' + port);
+})
