bc9671eacc91fdc3399f926bded6bff7c88a1e08
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-08-24 12:46:39 +0530

    Used Authenticatin as a custom middleware.

--- route/Login.js
+++ route/Login.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const login = express();
 
+login.use((req,res,next) => {
+    const {Authentication} = req.session;
+    if(Authentication || (req.path === '/' && req.method === 'POST') || (req.path === '/signup' && req.method === 'POST')){
+        next();
+    }else{
+        res.status(403).json({
+            Success: false,
+            Message: 'Authentication required.'
+        })
+    }
+})
+
 login.get('/', (req, res) => {
     res.json('login module.')
 })
