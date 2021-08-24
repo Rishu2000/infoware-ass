b87ab8d5101d7ef332f6cf38071503d9628382b0
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-08-24 12:33:58 +0530

    Implemented express-session.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+const session = require('express-session');
 const port = 5000;
 const login = require('./route/Login');
 
+app.use(session({
+    secret: 'infoware-ass',
+    resave: false,
+    saveUninitialized: false
+}))
 app.use('/login',login);
 app.get('/', (req, res) => {
     res.json('You are in root folder.');
