64ed0adc87bbdbbfc6656608d50877f49caf60bd
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-08-25 10:18:03 +0530

    Login module complete.

--- route/Login.js
+++ route/Login.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const login = express();
 const signup = require('./Signup');
+const {knex} = require('../pg/connection')
 
 login.use((req,res,next) => {
     const {Authentication} = req.session;
@@ -18,8 +19,41 @@ login.use('/signup',signup);
 login.get('/', (req, res) => {
     res.json('login module.')
 })
-login.post('/',(req,res) => {
-    res.json(req.body);
+login.post('/', async (req,res) => {
+    const {email,password} = req.body;
+    if(!email || !password){
+        req.session.destroy();
+        res.status(400).json({
+            Success:false,
+            Message:"Please enter both email and password"
+        });
+    }else{
+        await knex('users')
+        .where({email: email})
+        .then((rows) => {
+            if(rows.length > 0){
+                if(rows[0].password === password){
+                    req.session.Authentication = rows[0];
+                    res.json({
+                        Success:true,
+                        Message:rows[0].name
+                    });
+                }else{
+                    req.session.destroy();
+                    res.status(401).json({
+                        Success:false,
+                        Message:"Incorrect password."
+                    });
+                }
+            }else{
+                req.session.destroy();
+                res.status(404).json({
+                    Success:false,
+                    Message:"Please, signup if you are a new user."
+                });
+            }
+        })
+    }
 })
 
 module.exports = login;
