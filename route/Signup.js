fa079216bde0b932406fa11118e00e223993c012
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-08-25 10:13:43 +0530

    Signup module complete.

--- route/Signup.js
+++ route/Signup.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const signup = express();
+const {knex} = require('../pg/connection')
 
 signup.get('/',(req,res) => {
     res.json({
@@ -8,10 +9,37 @@ signup.get('/',(req,res) => {
     })
 })
 signup.post('/',(req,res) => {
-    res.json({
-        Success: true,
-        Message: req.body
-    })
+    const {username,email,password} = req.body;
+    if(!email || !password || !username){
+        req.session.destroy();
+        res.status(400).json({
+            Success:false,
+            Message:"All the fields are required."
+        });
+    }else{
+        knex('users')
+        .where({email: email})
+        .then((rows) => {
+            if(rows.length > 0){
+                req.session.destroy();
+                res.status(406).json({
+                    Success: false,
+                    Message:"email already exist."
+                })
+            }else{
+                knex('users')
+                .insert({name:username, password:password, email:email})
+                .then(() => {
+                    req.session.Authentication = req.body;
+                    res.status(201).json({
+                        Success: true,
+                        Message:username
+                    });
+                    // console.log(req.session);
+                })
+            }
+        })
+    }
 })
 
 module.exports = signup;
